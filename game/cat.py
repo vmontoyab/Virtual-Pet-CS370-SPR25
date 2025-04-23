90c3bb3b05c55e2bb498a49e6e67765e2c953ec9
Vanessa Montoya <vanessamontoya@Vanessas-MacBook-Air-2.local>
2025-04-22 23:32:31 -0600

    Add logic to split cat sprite sheet. Refactor purple circle to now be actual cat with default behavior walking. Pixelated background and removed unnecessary files.

--- game/cat.py
+++ game/cat.py
@@ -1,16 +1,34 @@
 import pygame
 from animal import Animal
+from sprite_sheet_processor import load_sprite_sheet
 
 class Cat(Animal):
-    def __init__(self, screen_width, screen_height, speed_x=2, speed_y=0, size=50):
+    def __init__(self, scale, screen_width, screen_height, speed_x=2, speed_y=0):
         super().__init__(screen_width, screen_height)
         self.speed_x = speed_x
         self.speed_y = speed_y
-        self.size = size
-        
-        floor_offset = 80
+        self.size = 64
+
+        floor_offset = 50
         self.y = (screen_height - floor_offset) - self.size
         self.x = screen_width // 2 - self.size // 2
+
+        self.animations = {
+            "right": load_sprite_sheet('images/Cats/Sprites/WalkRight.png', self.size, scale),
+            "left": load_sprite_sheet('images/Cats/Sprites/WalkLeft.png', self.size, scale)
+        }
+        self.facing = "right"
+        self.frames = self.animations[self.facing]
+
+        self.current_frame = 0
+        self.frame_timer = 0
+        self.frame_delay = 8
+    
+    def change_direction(self, direction):
+        self.facing = direction
+        self.frames = self.animations[direction]
+        self.speed_x *= -1
+
 
     def update(self, screen_width, screen_height):
         # Simple horizontal movement + bounce
@@ -20,16 +38,24 @@ class Cat(Animal):
 
         if self.x < wall_offset:
             self.x = wall_offset
-            self.speed_x *= -1
+
+            # Cat starts facing right
+            self.change_direction("right")
+
         elif self.x + self.size > screen_width-wall_offset:
             self.x = screen_width - wall_offset - self.size
-            self.speed_x *= -1
+
+            # Cat flips to face left 
+            self.change_direction("left")
 
     def draw(self, surface):
-        """
-        purple circle for now since I don't know how to like have a cat drawn
-        """
-        radius = self.size // 2
-        center_x = self.x + radius
-        center_y = self.y + radius
-        pygame.draw.circle(surface, (128, 0, 128), (center_x, center_y), radius)
+        # Get current frame in action
+        frame = self.frames[self.current_frame]
+        # Draw frame
+        surface.blit(frame, (self.x, self.y))
+
+        self.frame_timer += 1
+        if self.frame_timer >= self.frame_delay:
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.frame_timer = 0
+        
