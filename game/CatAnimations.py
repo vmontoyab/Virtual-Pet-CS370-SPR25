c6d3a8f829c9aecfdf97646eb2a79aab95054c97
Vanessa Montoya <vanessamontoya@Vanessas-MacBook-Air-2.local>
2025-04-24 00:19:01 -0600

    Implemented init for catAnimations

--- game/CatAnimations.py
+++ game/CatAnimations.py
@@ -1,7 +1,12 @@
 from sprite_sheet_processor import load_sprite_sheet 
 class CatAnimations:
     def _init_(self, scale):
-        pass
+        self.scale = scale
+        self.size = 64
+        self.animations = {
+            "walk_right": self.load("WalkRight.png"),
+            "walk_left": self.load("WalkLeft.png"),
+        }
 
     def load(self,filename):
         path = f"images/Cats/Sprites/{filename}"
